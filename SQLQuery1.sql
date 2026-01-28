de385dab93f499b50791356e21d1a217686d2c1f
hongan151206-ai <hongan151206@gmail.com>
2026-01-28 19:20:57 +0700

    Refactor SQL tables with drop and create statements
    Added drop statements for existing tables before creating new ones. Updated member, category, author, librarian, publisher, book, book_category, book_of_author, book_copy, administrator, policy_rule, and loan tables with specific changes.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,30 +1,42 @@
+drop table if exists member 
+GO
 create table member(
 	member_id varchar not null primary key,
-	name nvarchar,
-	address nvarchar,
+	name nvarchar(50),
+	address nvarchar(50),
 	phone_number float
 )
 
+drop table if exists category
+go
 create table category (
 	category_id varchar not null primary key,
 	category_name nvarchar
 )
 
+drop table if exists author
+go
 create table author (
 	author_id varchar not null primary key,
 	author_name nvarchar
 )
 
+drop table if exists librarian
+go
 create table librarian (
 	librarian_id varchar not null primary key,
 	librarian_name nvarchar
 )
 
+drop table if exists publisher
+go
 create table publisher (
 	publisher_id varchar not null primary key,
 	publisher_name nvarchar
 )
 
+drop table if exists book
+go
 create table book (
 	book_id varchar not null primary key,
 	publisher_id varchar,
@@ -35,6 +47,8 @@ create table book (
 	foreign key (librarian_id) references librarian(librarian_id)
 )
 
+drop table if exists book_category
+go
 create table book_category (
 	book_id varchar not null,
 	category_id varchar not null,
@@ -43,6 +57,8 @@ create table book_category (
 	primary key (book_id, category_id)
 )
 
+drop table if exists book_of_author 
+go
 create table book_of_author (
 	author_id varchar not null,
 	book_id varchar not null,
@@ -51,12 +67,16 @@ create table book_of_author (
 	primary key (author_id, book_id)
 )
 
+drop table if exists book_copy
+go
 create table book_copy (
 	book_copy_id varchar not null primary key,
 	book_id varchar,
 	foreign key (book_id) references book(book_id)
 )
 
+drop table if exists administrator
+go
 create table administrator (
 	admin_id varchar not null primary key,
 	admin_name nvarchar,
@@ -64,6 +84,8 @@ create table administrator (
 	phone_number float
 )
 
+drop table if exists policy_rule
+go
 create table policy_rule (
 	policy_id varchar not null primary key,
 	admin_id varchar not null,
@@ -74,6 +96,8 @@ create table policy_rule (
 	foreign key (admin_id) references administrator(admin_id)
 )
 
+drop table if exists loan
+go
 create table loan (
 	loan_id varchar not null primary key,
 	member_id varchar not null,
@@ -89,5 +113,6 @@ create table loan (
 	foreign key (librarian_id) references librarian(librarian_id),
 	foreign key (policy_id) references policy_rule(policy_id)
 )
+
 
 
